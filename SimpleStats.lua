136030d2fa278b4f018c6dbabe96336f8044d503
tony311 <tony@tonyfox.ws>
2014-12-18 18:07:42 -0500

    Clean up the INVTYPE mapping table declaration

--- SimpleStats.lua
+++ SimpleStats.lua
@@ -641,30 +641,32 @@ local function handleTooltip(self, ...)
 end
 
 local function setupTables()
-	invTypes = {}
-	invTypes["INVTYPE_HEAD"] = 1
-	invTypes["INVTYPE_NECK"] = 2
-	invTypes["INVTYPE_SHOULDER"] = 3
-	invTypes["INVTYPE_BODY"] = 4
-	invTypes["INVTYPE_CHEST"] = 5
-	invTypes["INVTYPE_ROBE"] = 5
-	invTypes["INVTYPE_WAIST"] = 6
-	invTypes["INVTYPE_LEGS"] = 7
-	invTypes["INVTYPE_FEET"] = 8
-	invTypes["INVTYPE_WRIST"] = 9
-	invTypes["INVTYPE_HAND"] = 10
-	invTypes["INVTYPE_FINGER"] = 11 -- also 12
-	invTypes["INVTYPE_TRINKET"] = 13 -- also 14
-	invTypes["INVTYPE_CLOAK"] = 15
-	invTypes["INVTYPE_WEAPON"] = 16 -- also 17
-	invTypes["INVTYPE_SHIELD"] = 17
-	invTypes["INVTYPE_2HWEAPON"] = 16
-	invTypes["INVTYPE_WEAPONMAINHAND"] = 16
-	invTypes["INVTYPE_WEAPONOFFHAND"] = 17
-	invTypes["INVTYPE_HOLDABLE"] = 17
-	invTypes["INVTYPE_RANGED"] = 16
-	invTypes["INVTYPE_RANGEDRIGHT"] = 16
-	invTypes["INVTYPE_TABARD"] = 19
+	invTypes = {
+		INVTYPE_HEAD = 1,
+		INVTYPE_NECK = 2,
+		INVTYPE_SHOULDER = 3,
+		INVTYPE_BODY = 4,
+		INVTYPE_CHEST = 5,
+		INVTYPE_ROBE = 5,
+		INVTYPE_WAIST = 6,
+		INVTYPE_LEGS = 7,
+		INVTYPE_FEET = 8,
+		INVTYPE_WRIST = 9,
+		INVTYPE_HAND = 10,
+		INVTYPE_FINGER = 11, -- also 12
+		INVTYPE_TRINKET = 13, -- also 14
+		INVTYPE_CLOAK = 15,
+		INVTYPE_WEAPON = 16, -- also 17
+		INVTYPE_SHIELD = 17,
+		INVTYPE_2HWEAPON = 16,
+		INVTYPE_WEAPONMAINHAND = 16,
+		INVTYPE_WEAPONOFFHAND = 17,
+		INVTYPE_HOLDABLE = 17,
+		INVTYPE_RANGED = 16,
+		INVTYPE_RANGEDRIGHT = 16,
+		INVTYPE_TABARD = 19,
+	}
+	
 	
 	local order_inverted = {
 		"RESISTANCE0_NAME",
