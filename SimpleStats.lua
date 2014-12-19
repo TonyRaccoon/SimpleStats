1c8b2c666003266f50b69526c3cfae4036020c33
tony311 <tony@tonyfox.ws>
2014-12-19 01:00:24 -0500

    Added print_r function for debugging

--- SimpleStats.lua
+++ SimpleStats.lua
@@ -1064,6 +1064,32 @@ function deepcopy(object)
 	return _copy(object)
 end
 end
+if not print_r then
+function print_r ( t ) 
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    sub_print_r(t,"  ")
+end
+end
 function __genOrderedIndex(t)
     local orderedIndex = {}
     for key in pairs(t) do table.insert( orderedIndex, key ) end
